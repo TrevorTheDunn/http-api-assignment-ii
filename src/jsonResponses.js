3c10c4e148a3a534d5db216a75e196c471198a43
TrevorTheDunn <tjd2695@rit.edu>
2024-02-17 18:51:14 -0500

    Added comments to code, also this will activate github actions

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,5 +1,9 @@
+// list of users, is temporary so will be reset whenever the server is reset
 const users = {};
 
+// respondJSOn function - takes in a request, response, status, and object
+// writes the response's head using the status code and headers json
+// writes a stringified version of the object json to the response
 const respondJSON = (request, response, status, object) => {
   const headers = {
     'Content-Type': 'application/json',
@@ -10,6 +14,8 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
+// responseJSONMeta function - takes in a request, response, and status code
+// writes only a head to the response using the headers json and the status code
 const respondJSONMeta = (request, response, status) => {
   const headers = {
     'Content-Type': 'application/json',
@@ -19,6 +25,10 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
+// getUsers function - takes in a request and response
+// constructs a responseJSON using the users list
+// passes the request and response into respondJSON as well as
+// a 200 status code and the responseJSON object
 const getUsers = (request, response) => {
   const responseJSON = {
     users,
@@ -27,8 +37,14 @@ const getUsers = (request, response) => {
   return respondJSON(request, response, 200, responseJSON);
 };
 
+// getUsersMeta function - takes in a request and response
+// passes the request and response into respondJSONMeta with a 200 status code
 const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 
+// notFound function - takes in a request and response
+// creates a JSON with a message and id to indicate the page wasn't found
+// passes the request and response to respondJSON with a 404 status code and 
+// the responseJSON object
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
@@ -38,10 +54,16 @@ const notFound = (request, response) => {
   return respondJSON(request, response, 404, responseJSON);
 };
 
+// notFoundMeta function - takes in a request and response
+// passes the request and response to respondJSONMeta with a 404 status code
 const notFoundMeta = (request, response) => {
   respondJSONMeta(request, response, 404);
 };
 
+// addUser function - takes in a request, response, and body
+// determines if the body is missing a name or age and responds accordingly
+// determines if the user is new or existing, and either updates the existing
+// user or creates a new one and handles that accordingly
 const addUser = (request, response, body) => {
   const responseJSON = {
     message: 'Name and age are both required.',
@@ -70,6 +92,7 @@ const addUser = (request, response, body) => {
   return respondJSONMeta(request, response, responseCode);
 };
 
+// public exports
 module.exports = {
   getUsers,
   getUsersMeta,
--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// urlStruct with Get and Head sections for different request methods, 
+// with the proper functions set for the proper urls
 const urlStruct = {
   GET: {
     '/': htmlHandler.getIndex,
@@ -24,6 +26,12 @@ const urlStruct = {
   },
 };
 
+// parseBody function - takes in a request, response, and a handler function
+// sets different functions for request actions, such as an error setting the 
+// response's status code to 404 and ending the response, data sending the 
+// chunk of data to the body, and when the request ends it parses the data 
+// of the body into parameters and sends them to the handler function 
+// with request and response
 const parseBody = (request, response, handler) => {
   const body = [];
 
@@ -45,12 +53,18 @@ const parseBody = (request, response, handler) => {
   });
 };
 
+// handlePost function: if the parsedUrl's pathname is /addUser, 
+// calls parseBody, passing in the addUser function from jsonHandler
 const handlePost = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/addUser') {
     parseBody(request, response, jsonHandler.addUser);
   }
 };
 
+// handleGet function: calls the method set in urlStruct under 
+// the request's method and the parsedUrl's pathname
+// if the pathname doesn't exist, calls the notFound method 
+// under the request's method
 const handleGet = (request, response, parsedUrl) => {
   if (urlStruct[request.method][parsedUrl.pathname]) {
     return urlStruct[request.method][parsedUrl.pathname](request, response);
@@ -59,6 +73,8 @@ const handleGet = (request, response, parsedUrl) => {
   return urlStruct[request.method].notFound(request, response);
 };
 
+//onRequest method: parses the request's url and calls either 
+// handlePost or handleGet depending on the request's method
 const onRequest = (request, response) => {
   // parse url into individual parts
   // returns an object of url parts by name
